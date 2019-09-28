96e55d79d9bf0fe3c4f9d722c2e784f3f4635663
Jan Maushagen <jan@wise67.vub.ac.be>
2019-09-28 18:25:29 +0200

    added dots

--- src/Map.js
+++ src/Map.js
@@ -3,15 +3,29 @@ import * as d3 from 'd3';
 import * as  topojson from 'topojson';
 import nyc from './nyc.json';
 // import * as d3Geo from 'd3-geo'
+import textures from 'textures';
+
+const SPEC = {"36047": {borough: 'Brooklyn', girl: 'Caro'}, "36085": {borough: 'Staten Island', girl:'Francesca'}, '36061': {borough: 'Manhattan', girl: 'Maude', }, '36081': {borough: 'Queens', girl: 'Verena'}, "36005": {borough: ' The Bronx', girl: 'Katharina'}}
+
+
 
 const map = (svgDom) => {
   var width = 660,
       height = 500;
 
+const t = textures.circles()
+  .radius(4)
+  .fill("darkorange")
+  .strokeWidth(2)
+  .stroke("firebrick")
+  .complement();
+;
+
   var boroughs = {36047: 'Brooklyn', 36085: 'Staten Island', 36061: 'Manhattan', 36081: 'Queens', 36005: 'The Bronx'};
+
   var surroundings = [['New Jersey',[-74.143982, 40.853792]], ['Long Island',[-73.648224, 40.738700]]];
 
-  var projection = d3.geoMercator()
+  var projection = d3.geoEquirectangular()
         .center([-73.96667, 40.78333])
         .scale(47000)
         .translate([310, 170]);
@@ -24,6 +38,8 @@ const map = (svgDom) => {
       .attr("height", height)
       .attr("viewBox", "0 0 660 500")
       .attr("perserveAspectRatio", "xMinYMid");
+
+      svg.call(t);
 
 
     var counties = topojson.feature(nyc, nyc.objects.counties).features;
@@ -38,7 +54,8 @@ const map = (svgDom) => {
     svg.selectAll(".county")
         .data(counties)
       .enter().append("path")
-        .attr("class", function(d) { return "county fips_" + d.id; })
+        // .attr("class", function(d) { return "county fips_" + d.id; })
+        .style('fill', t.url())
         .attr("d", path);
 
     svg.selectAll(".county-label")
